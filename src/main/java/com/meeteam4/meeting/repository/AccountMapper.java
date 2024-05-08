cd97f9df2a230acdbc779df88efdb5a4a39e7dce
JeonJuhwanaaa <w7285@naver.com>
2024-05-09 00:11:58 +0900

    fix:getTeacherProfile Mybatis

--- src/main/java/com/meeteam4/meeting/repository/AccountMapper.java
+++ src/main/java/com/meeteam4/meeting/repository/AccountMapper.java
@@ -55,7 +55,6 @@ public interface AccountMapper {
     public int deletePosterSubjectIds(@Param("posterId") int posterId);
     public int deletePosterClassTypeIds(@Param("posterId") int posterId);
 
-    public int deleteUser(@Param("userId") int userId);
 
     public int modifyStudentPoster(Poster poster);
 
