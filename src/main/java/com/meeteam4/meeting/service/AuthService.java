dcb8b5b90b15a869c27ddade52a19c585c5c19fc
JeonJuhwanaaa <w7285@naver.com>
2024-05-09 16:29:58 +0900

    fix:이메일중복확인

--- src/main/java/com/meeteam4/meeting/service/AuthService.java
+++ src/main/java/com/meeteam4/meeting/service/AuthService.java
@@ -18,6 +18,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 
 import java.util.Objects;
 
